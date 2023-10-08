99a4a80f1eec6507ae973ccdc7aeab64ecbcb7e8
Jhony <jash0310@gmail.com>
2023-10-08 00:52:18 -0500

    agregando token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,27 +1,57 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { RegisterDto } from './dto/register.dto';
+import * as bcryptjs from "bcryptjs"
+import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
 
   constructor(
-    private readonly userService: UsersService
+    private readonly userService: UsersService,
+    private readonly jwtService: JwtService
   ) { }
 
-  async login() {
-    return 'login'
-  }
 
-  async register(registerDto: RegisterDto) {
+  async register({ email, name, password }: RegisterDto) {
 
-    const findUser = await this.userService.findOneByEmail(registerDto.email)
+    const findUser = await this.userService.findOneByEmail(email)
     if (findUser) {
       throw new BadRequestException('Email ya existe!')
     }
-    const newUser = await this.userService.create(registerDto)
+    const newUser = await this.userService.create({
+      name,
+      email,
+      password: await bcryptjs.hash(password, 10)
+    })
 
     return newUser
   }
+
+  async login({ email, password }: LoginDto) {
+
+    const findUser = await this.userService.findOneByEmail(email)
+
+    if (!findUser) {
+      throw new UnauthorizedException('Email o Contraseña incorrectos')
+    }
+
+    const isPasswordValid = await bcryptjs.compare(password, findUser.password)
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Email o Contraseña incorrectos')
+    }
+
+    const payload = { email: findUser.email }
+
+    const token = await this.jwtService.signAsync(payload)
+
+    return {
+      token,
+      email
+    }
+  }
+
 
 }
---
+++ src/auth/constants/jwt.constant.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+  secret: "Palabra_secreta"
+}
