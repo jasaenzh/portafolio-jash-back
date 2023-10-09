3a7e21927c79984a3d0ea6dc0c085dee9b451146
Jhony <jash0310@gmail.com>
2023-10-08 23:34:26 -0500

    extraer token y devolver un payload

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
@@ -23,8 +23,8 @@ export class AuthController {
 
   @Get('profile')
   @UseGuards(AuthGuard)
-  profile() {
-    return 'profile'
+  profile(@Request() req) {
+    return req.user
   }
 
 }
